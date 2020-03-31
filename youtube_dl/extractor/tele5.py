fd313604346f465eb267aab4fac286c040fbfafe
AndrewMBL <62922222+AndrewMBL@users.noreply.github.com>
2020-04-01 01:52:43 +1100

    Replace NexxIE with JWPlatformIE
    Replaces the NexxIE with JWPlatformIE as that made extracting the video id simpler.

--- youtube_dl/extractor/tele5.py
+++ youtube_dl/extractor/tele5.py
@@ -2,29 +2,40 @@
 from __future__ import unicode_literals
 
 from .common import InfoExtractor
-from .nexx import NexxIE
+from .jwplatform import JWPlatformIE
 from ..compat import compat_urlparse
 
 
 class Tele5IE(InfoExtractor):
     _VALID_URL = r'https?://(?:www\.)?tele5\.de/(?:[^/]+/)*(?P<id>[^/?#&]+)'
     _TESTS = [{
-        'url': 'https://www.tele5.de/mediathek/filme-online/videos?vid=1549416',
+        'url': 'https://www.tele5.de/filme/mega-alligators/',
         'info_dict': {
-            'id': '1549416',
+            'id': 'X7QbljEQ',
             'ext': 'mp4',
-            'upload_date': '20180814',
-            'timestamp': 1534290623,
-            'title': 'Pandorum',
+            'upload_date': '20200330',
+            'timestamp': 1585605000,
+            'title': 'Mega Alligators',
+            'description': 'md5:c9797a326f95f97fd466d453d63a92c1',
         },
         'params': {
             'skip_download': True,
         },
     }, {
-        'url': 'https://www.tele5.de/kalkofes-mattscheibe/video-clips/politik-und-gesellschaft?ve_id=1551191',
-        'only_matching': True,
+        'url': 'https://www.tele5.de/kalkofes-welt/best-of-clips/politik-und-gesellschaft/?ve_id=9MD418FO',
+        'info_dict': {
+            'id': '9MD418FO',
+            'ext': 'mp4',
+            'upload_date': '20200326',
+            'timestamp': 1585189280,
+            'title': 'Panorama - Merkel muss weg!',
+            'description': 'md5:3bdf72fb093d1e8b01f515b0ee1dec9e',
+        },
+        'params': {
+            'skip_download': True,
+        },
     }, {
-        'url': 'https://www.tele5.de/video-clip/?ve_id=1609440',
+        'url': 'https://www.tele5.de/fsk-sex/?ve_id=1662408',
         'only_matching': True,
     }, {
         'url': 'https://www.tele5.de/filme/schlefaz-dragon-crusaders/',
@@ -36,9 +47,10 @@ class Tele5IE(InfoExtractor):
         'url': 'https://www.tele5.de/star-trek/raumschiff-voyager/ganze-folge/das-vinculum/',
         'only_matching': True,
     }, {
-        'url': 'https://www.tele5.de/anders-ist-sevda/',
+        'url': 'https://www.tele5.de/kalkofes-welt/',
         'only_matching': True,
     }]
+
 
     def _real_extract(self, url):
         qs = compat_urlparse.parse_qs(compat_urlparse.urlparse(url).query)
@@ -47,11 +59,10 @@ class Tele5IE(InfoExtractor):
         if not video_id:
             display_id = self._match_id(url)
             webpage = self._download_webpage(url, display_id)
-            video_id = self._html_search_regex(
-                (r'id\s*=\s*["\']video-player["\'][^>]+data-id\s*=\s*["\'](\d+)',
-                 r'\s+id\s*=\s*["\']player_(\d{6,})',
-                 r'\bdata-id\s*=\s*["\'](\d{6,})'), webpage, 'video id')
+            video_id = self._search_regex(
+                (r'id\s*=\s*["\']video-player["\'][^>]+data-id\s*=\s*["\']([^"]+)',
+                 r'\s+id\s*=\s*["\']player_([^"]{6,})',
+                 r'\bdata-id\s*=\s*["\']([^"]{6,})'), webpage, 'video_id')
 
         return self.url_result(
-            'https://api.nexx.cloud/v3/759/videos/byid/%s' % video_id,
-            ie=NexxIE.ie_key(), video_id=video_id)
+            'jwplatform:%s' % video_id, ie=JWPlatformIE.ie_key())
